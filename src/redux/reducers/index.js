1f6478f0412bc3997d086236b6405e2f28d9bb48
Ivan Bitskevych <ibits@softserveinc.com>
2023-08-14 10:21:51 +0300

    UC#8 React initial setup

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,7 +3,6 @@ import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
     user: userReducer
-    // other reducers go here
 });
 
 export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import rootReducer from './reducers';
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  // This line is optional but it helps you use Redux DevTools
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
