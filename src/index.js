9328c53a27a2a20bf314c9eb1e6e0104cab9e9d8
Ivan Bitskevych <ibits@softserveinc.com>
2023-08-14 12:13:12 +0300

    UC#8 cleanup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import store from './redux/store';
 import './index.css';
 import FormComponent from "./FormComponent";
 import DisplayData from "./DisplayData";
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,8 +15,3 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
